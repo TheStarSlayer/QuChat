bf0c7fa7eae43fc149679df3ced826f77025b9ee
TheStarSlayer <113292968+TheStarSlayer@users.noreply.github.com>
2026-02-24 18:54:22 +0530

    Add refresh controller to authRoute

--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -1,11 +1,13 @@
 import express from "express";
-import { loginController, signupController, logoutController } from "../controllers/auth.controller.js";
+import { loginController, signupController, 
+         refreshController, logoutController } from "../controllers/auth.controller.js";
 
 const authRoute = express.Router();
 
 // TODO: Implement these controllers!
 authRoute.post("/login", loginController);
 authRoute.post("/signup", signupController);
+authRoute.put("/refresh", refreshController)
 authRoute.get("/logout", logoutController);
 
 export default authRoute;
