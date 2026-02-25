c2be87a9a633c88764c9e9cc4486a3aa74cfff15
TheStarSlayer <113292968+TheStarSlayer@users.noreply.github.com>
2026-02-25 12:53:26 +0530

    Remove /verify path from authRouter

--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -1,11 +1,9 @@
 import express from "express";
-import { verifyStateController,
-         loginController, signupController, 
+import { loginController, signupController, 
          refreshController, logoutController } from "../controllers/auth.controller.js";
 
 const authRouter = express.Router();
 
-authRouter.get("/verify", verifyStateController);
 authRouter.post("/login", loginController);
 authRouter.post("/signup", signupController);
 authRouter.post("/refresh", refreshController)
