d3854d67dd602612f1958c79119377650a343474
TheStarSlayer <113292968+TheStarSlayer@users.noreply.github.com>
2026-03-05 18:53:12 +0530

    Add /api middleware to check access token

---
+++ backend/middleware/api.middleware.js
@@ -0,0 +1,17 @@
+import jwt from "jsonwebtoken";
+
+export const apiVerify = async (req, res, next) => {
+    const authHeader = req.headers.authorization;
+    const [bearer, token] = authHeader.split(' ');
+
+    if (bearer !== "Bearer" || !token)
+        return res.status(401).json({ error: 'Invalid auth token' });
+
+    try {
+        jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+        next();
+    }
+    catch (error) {
+        return res.status(401).json({ error: 'Invalid auth token' });
+    }
+};
