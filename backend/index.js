cdc1d348d7af0f88151c853cc708f692e40395a5
TheStarSlayer <113292968+TheStarSlayer@users.noreply.github.com>
2026-03-07 13:35:05 +0530

    Add socket.io functionality Add redis client connection Move mongodb conn initiation to lib/dbConnect.js

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,17 @@
 import express from "express";
 import cookieParser from "cookie-parser";
 import "dotenv/config";
-import mongoose from "mongoose";
+import { mongoConnect, redisConnect } from "./lib/dbConnect.js";
 import authRouter from "./routes/auth.route.js";
+import { Server } from "socket.io";
 
 const app = express();
 const PORT = 8596;
+const io = new Server(8597, {
+    cors: {
+        origin: ['http://localhost:8596',]
+    }
+});
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -13,13 +19,15 @@ app.use(cookieParser());
 
 app.use("/auth", authRouter);
 
+// Socket.io event listening
+io.on("connection", async socket => {
+    const client = await redisConnect();
+    socket.on('connect', async userId => {
+        await client.set(userId, socket.id);
+    });
+});
+
 app.listen(PORT, () => {
     console.log(`App started on PORT ${PORT}`);
-    try {
-        mongoose.connect(process.env.MONGODB_CONN);
-    }
-    catch (err) {
-        console.error("Unexpected error occurred: Database could not be connected.");
-        console.error(err.message);
-    }
+    mongoConnect();
 });
