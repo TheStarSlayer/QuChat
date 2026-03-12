fbb33219a313324f6ba543e68c7e1ad36290a5f5
TheStarSlayer <113292968+TheStarSlayer@users.noreply.github.com>
2026-03-12 15:10:37 +0530

    Edit onlineUsersSchema: remove socketId; add isBusy

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -11,7 +11,7 @@ const userSchema = new Schema({
 
 const onlineUsersSchema = new Schema({
     username: String,
-    socketId: String
+    isBusy: Boolean
 });
 
 const User = model('User', userSchema);
