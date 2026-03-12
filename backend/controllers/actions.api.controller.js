d892a22ae88669ead16026c258be98495224a44f
TheStarSlayer <113292968+TheStarSlayer@users.noreply.github.com>
2026-03-12 15:43:40 +0530

    Add setToBusy and setToAvailable routes and controllers

--- backend/controllers/actions.api.controller.js
+++ backend/controllers/actions.api.controller.js
@@ -1,4 +1,7 @@
 import retrieveOnlineUsers from "../lib/retrieveOnlineUsers.js";
+import { redisClient } from "../index.js";
+import io from "../io.index.js";
+import { OnlineUsers } from "../models/user.model.js";
 
 export const verifyAccessTokenController = (_, res) => {
     return res.status(200).json({ msg: "Access token verified successfully!" });
@@ -10,3 +13,37 @@ export const getOnlineUsersController = async (_, res) => {
         return res.status(500).json({ error: "Internal Server Error" });
     return res.status(200).json({ onlineUsers });
 }
+
+export const setToBusyController = async (req, res) => {
+    const userId = req.userId;
+
+    try {
+        await redisClient.hSet('onlineUsers', userId, true);
+        await OnlineUsers.updateOne({ username: userId }, { isBusy: true });
+
+        io.emit('userLeft', userId);
+    }
+    catch (err) {
+        console.error("Unexpected error occurred", err.message);
+        return res.status(500).json({ error: "Internal Server Error" });
+    }
+
+    return res.status(200).json({ msg: "User set to busy successfully!" });
+}
+
+export const setToAvailableController = async (req, res) => {
+    const userId = req.userId;
+
+    try {
+        await redisClient.hSet('onlineUsers', userId, false);
+        await OnlineUsers.updateOne({ username: userId }, { isBusy: false });
+
+        io.emit('newUser', userId);
+    }
+    catch (err) {
+        console.error("Unexpected error occurred", err.message);
+        return res.status(500).json({ error: "Internal Server Error" });
+    }
+
+    return res.status(200).json({ msg: "User set to available successfully!" });
+};
--- backend/routes/api.route.js
+++ backend/routes/api.route.js
@@ -3,12 +3,17 @@ import {
     persistRequestController, finishRequestController,
     eavesdroppableRequestsController, eavesdropController
 } from "../controllers/request.api.controller.js";
-import { getOnlineUsersController, verifyAccessTokenController } from "../controllers/actions.api.controller.js";
+import {
+    getOnlineUsersController, verifyAccessTokenController,
+    setToBusyController, setToAvailableController
+} from "../controllers/actions.api.controller.js";
 
 const apiRouter = express.Router();
 
 apiRouter.get("/verify", verifyAccessTokenController);
 apiRouter.get("/getOnlineUsers", getOnlineUsersController);
+apiRouter.patch("/setToBusy", setToBusyController);
+apiRouter.patch("/setToAvailable", setToAvailableController);
 
 apiRouter.post("/persistRequest", persistRequestController);
 apiRouter.get("/getEavesdroppableRequests", eavesdroppableRequestsController);
