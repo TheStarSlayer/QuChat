8ebb5a6c20acdaec5fdf1907d49acfc41a681730
TheStarSlayer <113292968+TheStarSlayer@users.noreply.github.com>
2026-03-31 18:33:28 +0530

    Add logout function

---
+++ frontend/src/lib/logout.js
@@ -0,0 +1,15 @@
+import authCaller from "./auth";
+import { toast } from "react-toastify";
+
+export default async function logout(navigate) {
+    try {
+        await authCaller.post("/logout");
+        localStorage.removeItem("access-token");
+        navigate("/onboard");
+    }
+    catch (error) {
+        if (error.response.status === 500) {
+            toast.error("Could not logout -- internal server error");
+        }
+    }
+}
