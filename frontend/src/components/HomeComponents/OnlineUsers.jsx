14c4fc8783960ee8e6db9edd83cb9c131cc983c4
TheStarSlayer <113292968+TheStarSlayer@users.noreply.github.com>
2026-04-02 11:26:01 +0530

    Implement searcher and newRequest fns in OnlineUsers.jsx; Provide comments and stub for collaborators

---
+++ frontend/src/components/HomeComponents/OnlineUsers.jsx
@@ -0,0 +1,37 @@
+function OnlineUsers({ onlineUsers, searchTerm, setSearchTerm, setShowNewRequest }) {
+    let subsetOnlineUsers = [...onlineUsers];
+
+    function searcher() {
+        if (searchTerm === "") {
+            subsetOnlineUsers = [...onlineUsers];
+        }
+        else {
+            const regex = new RegExp(searchTerm, "i");
+            subsetOnlineUsers = onlineUsers.filter((user) => regex.test(user.username));
+        }
+    }
+
+    function newRequest(receiverUserId) {
+        setShowNewRequest(receiverUserId);
+    }
+
+    /**
+     * Contains list of online users with request button (for chatting)
+     * 
+     * Contains a search bar (for searching through onlineUsers)
+     * 
+     * When searchbar is empty, show all users (initially, all are shown)
+     * When value of searchbar changes (use searchTerm state), call setSearchTerm and searcher() function
+     * 
+     * Build the list from subsetOnlineUsers only
+     * 
+     * When request button is clicked, call newRequest(receiverUserId) -> 
+     *      try to set receiverUserId based on list key
+     */
+    return (
+        <>
+        </>
+    );
+}
+
+export default OnlineUsers;
