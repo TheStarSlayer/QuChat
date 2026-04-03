2515bb79f3f82ce03afd524ac14d70e83a3b0100
TheStarSlayer <113292968+TheStarSlayer@users.noreply.github.com>
2026-04-03 17:20:44 +0530

    Write functions for RequestsToMe

--- frontend/src/components/HomeComponents/ChatWindowComponents/RequestsToMe.jsx
+++ frontend/src/components/HomeComponents/ChatWindowComponents/RequestsToMe.jsx
@@ -1,5 +1,59 @@
+import HomeContext from "../../../contexts/HomeContext";
+import { useContext } from "react";
+
 function RequestsToMe() {
+    /**
+     * Use subsetRequestsToMe to build a list
+     * 
+     * It contains searchbar to search through requests based on sender/host
+     * It contains a close button that calls resetChatWindow
+     * Each list item (request) contains request details and two buttons: Accept and Reject
+     * The arguments/parameters need to be modified based on how the request is passed to the function
+     * Currently assuming request to be an object (you can choose to pre-process it into an object...)
+     * If accept, response is true; else, response is false
+     */
     
+    const {
+        searchTermForRTM, setSearchTermForRTM,
+        requestsToMe, resetChatWindow, socketRef,
+        initChatSession, setShowChatSession,
+        setRequestsToMe
+    } = useContext(HomeContext);
+
+    let subsetRequestsToMe = [...requestsToMe];
+
+    function searcher() {
+        if (searchTermForRTM === "") {
+            subsetRequestsToMe = [...requestsToMe];
+        }
+        else {
+            const regex = new RegExp(searchTermForRTM, "i");
+            subsetRequestsToMe = requestsToMe.filter((request) => regex.test(request.sender));
+        }
+    }
+
+    async function respondToRequest(request, response) {
+        const socket = socketRef.current;
+
+        setRequestsToMe(requests =>
+            requests.filter(requestToMe => requestToMe.sender !== request.sender));
+        
+        if (!response) {
+            socket.emit("reject", request.sender);
+            return;
+        }
+
+        socket.emit("accept", request.sender, request.typeOfEncryption);
+        
+        resetChatWindow();
+        initChatSession(request.sender, request.typeOfEncryption, request.chatSessionTimeInMin, "receiver");
+        setShowChatSession();
+    }
+
+    return (
+        <>
+        </>
+    );
 }
 
 export default RequestsToMe;
