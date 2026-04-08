f9daa174abf260f1e13ead724b276fb034565521
TheStarSlayer <113292968+TheStarSlayer@users.noreply.github.com>
2026-04-08 18:34:47 +0530

    Introduce delay before making request to qc for better view of status window msg

--- frontend/src/lib/qc.js
+++ frontend/src/lib/qc.js
@@ -8,11 +8,15 @@ const qcCaller = axios.create({
     withCredentials: true
 });
 
+const delay = (ms) => new Promise(resolve => setTimeout(resolve, ms));
+
 // Add auth token dynamically on every request
-qcCaller.interceptors.request.use((config) => {
+qcCaller.interceptors.request.use(async (config) => {
     const token = localStorage.getItem("access-token");
     if (token)
         config.headers.Authorization = token;
+
+    await delay(500);
     return config;
 });
 
