bb7fbd44124347d0da1881f315bd0943a5427f57
TheStarSlayer <113292968+TheStarSlayer@users.noreply.github.com>
2026-04-10 00:52:51 +0530

    Use defaultConfig

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,30 +1,26 @@
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
-export default ({ mode }) => {
-    const env = loadEnv(mode, process.cwd());
-
-    return defineConfig({
-        plugins: [react(), tailwindcss()],
-        server: { 
-            port: 8595,
-            proxy: {
-                "/auth": {
-                    target: "http://localhost:8596",
-                    changeOrigin: true
-                },
-                "/api": {
-                    target: "http://localhost:8596",
-                    changeOrigin: true
-                },
-                "/socket.io": {
-                    target: "http://localhost:8596",
-                    ws: true,
-                    changeOrigin: true
-                }
+export default defineConfig({
+    plugins: [react(), tailwindcss()],
+    server: { 
+        port: 8595,
+        proxy: {
+            "/auth": {
+                target: "http://localhost:8596",
+                changeOrigin: true
+            },
+            "/api": {
+                target: "http://localhost:8596",
+                changeOrigin: true
+            },
+            "/socket.io": {
+                target: "http://localhost:8596",
+                ws: true,
+                changeOrigin: true
             }
         }
-    });
-};
+    }
+});
